e6d7ddac23a2662bc1626f1c0539785de409b26e
kindarabbit <dbsgk4747@gmail.com>
2025-08-13 18:40:41 +0900

    main_0813

--- app/src/main/java/com/example/autovent_2025/UI/LoginActivity.java
+++ app/src/main/java/com/example/autovent_2025/UI/LoginActivity.java
@@ -9,7 +9,7 @@ import com.example.autovent_2025.UI.main.MainActivity;
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
