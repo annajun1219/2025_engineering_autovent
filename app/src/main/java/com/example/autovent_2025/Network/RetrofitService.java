5ce2607c5e283d8ed5a49d87ab4c84b0ab7dd095
annajun1219 <mynameisanna202@gmail.com>
2025-08-14 15:18:27 +0900

    [fetch] login api
    로그인 api연동

--- app/src/main/java/com/example/autovent_2025/Network/RetrofitService.java
+++ app/src/main/java/com/example/autovent_2025/Network/RetrofitService.java
@@ -1,10 +1,14 @@
+// RetrofitService.java
 package com.example.autovent_2025.Network;
+
+import com.example.autovent_2025.Model.LoginRequest;
+import com.example.autovent_2025.Model.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
-// 로그인 API 호출용 인터페이스
 public interface RetrofitService {
-
+    @POST("/auth/login")
+    Call<LoginResponse> login(@Body LoginRequest request);
 }
